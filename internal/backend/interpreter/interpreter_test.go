ab5fc052073f8805b6fc03856c8eae5d91c06389
s-ginger <jum80frol@gmail.com>
2026-08-19 18:57:47 +0600

    modified:   internal/backend/interpreter/interpreter_test.go

--- internal/backend/interpreter/interpreter_test.go
+++ internal/backend/interpreter/interpreter_test.go
@@ -1,13 +1,19 @@
 package interpreter
 
-
 import (
+	"j++/internal/lexer"
+	"j++/internal/parser"
 	"testing"
 )
 
 func TestInterpreter_Eval(t *testing.T) {
-
 	
+	l := lexer.NewLexer("1 2 4 [2 4] len print")
+	p := parser.NewParser(l.Tokenize())
+	ast := p.Parse()
+	i := NewInterpreter(Stack{})
+
+	i.Eval(ast)
 
 }
 
